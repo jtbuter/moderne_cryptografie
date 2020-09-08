7773be4dcc9826dc5dcfe61e9b54202b2e41fee4
Joël Buter <jtbuter@gmail.com>
2020-09-08 15:15:39 +0200

    key length automatically determined

--- week1/decrypt.py
+++ week1/decrypt.py
@@ -3,18 +3,20 @@ import numpy as np
 def decode(cipher, n):
 	return list(map(lambda x: x ^ n, cipher))	
 
-def decrypt(n, cipher):
-	frequencies = np.array([0] * 256)
-
-	for c in cipher[::n]:
-		frequencies[c] += 1
-
-	return np.sum((frequencies / np.sum(frequencies)) ** 2)
-
 def get_length(cipher):
-	f = np.vectorize(lambda n: decrypt(n, cipher))
-	
-	return np.argmax(f(range(1, 14))) + 1
+	length_bag = np.array([0.] * 14)
+
+	for n in range(1, 14):
+		frequency_bag = np.array([0] * 256)
+		stream = cipher[::n]
+
+		for c in stream:
+			frequency_bag[c] += 1
+
+		length_bag[n] = np.sum((frequency_bag / len(stream)) ** 2)
+
+	return np.argmax(length_bag)
+
 
 def get_key(cipher, length):	
 	key = []
@@ -34,8 +36,8 @@ def get_key(cipher, length):
 				
 				if c >= 97 and c <= 122:
 					frequency_bag[c - 97] += 1
-				if c == 32:
-					frequency_bag[c - 6] += 1
+				elif c == 32:
+					frequency_bag[26] += 1
 
 			if invalid_message_space:
 				continue
